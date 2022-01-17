ddee930b3c1bd0726434a7dbe9336d2739d8e01f
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2022-01-18 00:23:32 +0100

    Update to Apodini 0.7.1 (#5)

--- Package.swift
+++ Package.swift
@@ -14,13 +14,13 @@ import PackageDescription
 let package = Package(
     name: "ApodiniObservePrometheus",
     platforms: [
-        .macOS(.v12)
+        .macOS(.v11)
     ],
     products: [
         .library(name: "ApodiniObservePrometheus", targets: ["ApodiniObservePrometheus"])
     ],
     dependencies: [
-        .package(url: "https://github.com/Apodini/Apodini.git", .upToNextMinor(from: "0.6.1")),
+        .package(url: "https://github.com/Apodini/Apodini.git", .upToNextMinor(from: "0.7.1")),
         .package(url: "https://github.com/apple/swift-metrics.git", .upToNextMinor(from: "2.2.0")),
         .package(url: "https://github.com/MrLotU/SwiftPrometheus.git", from: "1.0.0-alpha")
     ],
